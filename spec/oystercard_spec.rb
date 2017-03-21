90706172119735160b916a96138fb492786c5096
Jean-Jacques Calbayrac <calbayracjj@gmail.com>
2017-03-21 10:24:16 +0000

    Adds a balance to the card

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,3 +1,12 @@
+require "oystercard"
+
 describe Oystercard do
-  
+  subject(:oystercard) {described_class.new}
+  # In order to use public transport
+  # As a customer
+  # I want money on my card
+
+  it "want a default balance of 0 on the card" do
+    expect(oystercard.balance).to eq 0
+  end
 end
