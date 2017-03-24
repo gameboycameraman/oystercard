f827681c93fd246fb00ab60a1015460dc5ceaf66
Colin Turner <colinturner34@gmail.com>
2017-03-24 11:08:06 +0000

    Add failing test for resetting entry station to nil on finish

--- lib/journey.rb
+++ lib/journey.rb
@@ -1,3 +1,10 @@
 class Journey
 
+  attr_reader :entry_station
+  attr_writer :entry_station
+
+  def start(start_station)
+    @entry_station = start_station
+  end
+
 end
--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -4,19 +4,27 @@ require 'journey'
 describe Journey do
 subject(:journey) {described_class.new}
 
-let(:station) {double :station}
+let(:start_station) {double :station}
 
   describe "#start" do
-
     # in order to be correctly charged
     # as a customer
     # record entry station
-
-    it "assigns an entry station" do
-      journey.start(station)
-      expect(journey.station).to eq station
+    it "records an entry station" do
+      journey.start(start_station)
+      expect(journey.entry_station).to eq start_station
     end
+  end
 
+  describe "#finish" do
+    # in order to be correctly charged
+    # as a customer
+    # record exit station
+    it "resets the entry_station to nil" do
+      journey.start(start_station)
+      journey.finish(finish_station)
+      expect(journey.entry_station).to be_nil
+    end
   end
 
 end
